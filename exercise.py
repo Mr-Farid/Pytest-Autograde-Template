fabd3d78d44779d3af21ac5de2bdd3badd9c9e74
Mr-Farid <71142299+Mr-Farid@users.noreply.github.com>
2020-12-09 13:07:00 -0500

    clarified exercise file

--- exercise.py
+++ exercise.py
@@ -1,7 +1,12 @@
 def main():
-    #write your code below this line, do not delete any of the other lines
-    x = input('what to add 2 to?')
-    print(2 + int(x))
+    # write your code below this line, do not delete any of the other lines
+    # make sure its tabbed over as a block of the 'def main() function'
+    x = input('example code with input')
+
+
+
+# Do not touch anything below this line, this is to run the function when
+# running this file
 
 if __name__ == '__main__':
     main()
